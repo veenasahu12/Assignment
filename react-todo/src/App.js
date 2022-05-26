a69afb3c402557050dca7d119c1f8f049dc1b0ef
veenasahu12 <veenasahusbp@gmail.com>
2022-05-27 01:02:54 +0530

    submit

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Todo from './components/Todo';
+import Todos from './components/TodoApp/Todos';
 
 function App() {
    return (
       <div className="App">
          <h1>TODO</h1>
          <Todo />
+         <br />
+         <Todos/>
       </div>
    );
 }
---
+++ react-todo/src/components/TodoApp/Todos.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import "./todos.css"
+
+const Todos = () =>  {
+    
+    const [todo,setTodo] = useState([])
+    const [newtodo,setNewTodo] = useState("")
+
+    const saveinfo = () => {
+        fetch("http://localhost:8080/todo",{
+            method: "POST",
+            headers:{
+                "content-type" : "application/JSON"
+            },
+            body : JSON.stringify({
+               text : newtodo,
+               done : false,
+            })
+        })
+        .then((r) => r.json())
+        .then((d) => {
+            setTodo([[...todo , d]])
+            setNewTodo("")
+        })
+    }
+
+useEffect(()=>{
+    fetch("http://localhost:8080/todo?_page=1&_limit=5")
+        .then((r) => r.json())
+        .then((d) => {
+            // console.log(d);
+            setTodo(d)
+        })
+    },[])
+    return (
+        <div className='maindiv'>
+            <h1>Fetch Todos</h1>
+            <input value={newtodo} onChange={({target}) => setNewTodo(target.value)}/>
+            <button onClick={saveinfo}>Add</button>
+            {todo.map((todo) => {
+                return <div key={todo.id}>{todo.text}</div>
+            })}
+        </div>
+    )
+}
+
+export default Todos
